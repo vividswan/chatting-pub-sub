9712e22659008ea23600c94e9b8b5f099195f0b1
vividswan <vividswan@naver.com>
2024-04-06 22:39:25 +0900

    first init

---
+++ src/main/kotlin/com/vividswan/chattingpubsub/ChattingPubSubApplication.kt
@@ -0,0 +1,11 @@
+package com.vividswan.chattingpubsub
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ChattingPubSubApplication
+
+fun main(args: Array<String>) {
+	runApplication<ChattingPubSubApplication>(*args)
+}
---
+++ src/test/kotlin/com/vividswan/chattingpubsub/ChattingPubSubApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.vividswan.chattingpubsub
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ChattingPubSubApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
