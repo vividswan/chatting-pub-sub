fb4f8f73f48471c20643ff42efb07d5ea145f694
vividswan <vividswan@naver.com>
2024-04-30 12:54:24 +0900

    feat: enterChatRoom 메서드 추가

--- src/main/kotlin/com/vividswan/chattingpubsub/service/ChatService.java
+++ src/main/kotlin/com/vividswan/chattingpubsub/service/ChatService.java
@@ -1,11 +1,24 @@
 package com.vividswan.chattingpubsub.service;
 
+import java.util.Scanner;
+
 import org.springframework.data.redis.connection.Message;
 import org.springframework.data.redis.connection.MessageListener;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ChatService implements MessageListener {
+
+	public void enterChatRoom(String roomName) {
+		Scanner scanner = new Scanner(System.in);
+		while (scanner.hasNextLine()) {
+			String line = scanner.nextLine();
+			if (line.equals("q")) {
+				System.out.println("Quit");
+				break;
+			}
+		}
+	}
 
 	@Override
 	public void onMessage(Message message, byte[] pattern) {
