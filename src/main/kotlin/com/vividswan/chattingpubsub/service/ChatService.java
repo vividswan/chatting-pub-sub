0f2dd45fc3cccc6f907e9333f0f91038ce2105ea
vividswan <vividswan@naver.com>
2024-05-02 00:32:52 +0900

    feat: Service에 컨테이너 주입

--- src/main/kotlin/com/vividswan/chattingpubsub/service/ChatService.java
+++ src/main/kotlin/com/vividswan/chattingpubsub/service/ChatService.java
@@ -4,12 +4,21 @@ import java.util.Scanner;
 
 import org.springframework.data.redis.connection.Message;
 import org.springframework.data.redis.connection.MessageListener;
+import org.springframework.data.redis.listener.ChannelTopic;
+import org.springframework.data.redis.listener.RedisMessageListenerContainer;
 import org.springframework.stereotype.Service;
 
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
 @Service
 public class ChatService implements MessageListener {
 
+	private final RedisMessageListenerContainer container;
+
 	public void enterChatRoom(String roomName) {
+		container.addMessageListener(this, new ChannelTopic(roomName));
+
 		Scanner scanner = new Scanner(System.in);
 		while (scanner.hasNextLine()) {
 			String line = scanner.nextLine();
